a9cceb98a65fd2f74444d4f8df7f71e8d1a4488a
shitalphadtare <68630362+shitalphadtare@users.noreply.github.com>
2020-08-28 12:51:06 +0530

    Add files via upload

---
+++ SEALINKERES/HANA/Ledger/Bank Reconciliation Report/PTS_BALANCE_PER_BANKTOTAL.sql
@@ -0,0 +1,13 @@
+
+
+CREATE VIEW PTS_BALANCE_PER_BANKTOTAL
+AS
+select  obnk."AcctCode" as "Mthacctcod",
+		obnk."DueDate" as "Matchdate",
+		(obnk."CredAmnt"-obnk."DebAmount")as "totals"	
+		,obnk."AcctName"						
+from obnk							
+Left Join omth on obnk."BankMatch"=omth."MatchNum" and obnk."AcctCode" = omth."MthAcctCod"							
+
+
+
